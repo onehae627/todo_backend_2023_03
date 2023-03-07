3eceebaa4152b8fffe938157aa6939cfbffc3228
onehae627 <onehae627@gmail.com>
2023-03-07 21:38:11 +0900

    cors 허용

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+    origin: "https://naver.io",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+  
+  app.use(cors(corsOptions));
+
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/todos", (req, res) => {
+    res.json([{id : 1 }, {id: 2 }]);
+  });
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
