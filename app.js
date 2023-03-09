a52cb2bb2004ab0d3991ac5d8e554f0e69ba2bf4
onehae627 <onehae627@gmail.com>
2023-03-09 20:39:42 +0900

    생성

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const pool = mysql.createPool({
 });
 
 const app = express();
+app.use(express.json());
 
 const corsOptions = {
     origin: "https://naver.io",
@@ -109,6 +110,69 @@ app.get("/:user_code/todos", async (req, res) => {
       msg : `${no}번 할 일을 삭제하였습니다.`,
     });
   });
+
+  app.post("/:user_code/todos", async (req, res) => {
+    const { user_code } = req.params;
+  
+    const { content, perform_date, is_completed = 0 } = req.body;
+  
+    if (!content) {
+      res.status(400).json({
+        resultCode: "F-1",
+        msg: "content required",
+      });
+    }
+  
+    if (!perform_date) {
+      res.status(400).json({
+        resultCode: "F-1",
+        msg: "perform_date required",
+      });
+    }
+  
+    const [[lastTodoRow]] = await pool.query(
+      `
+      SELECT no
+      FROM todo
+      WHERE user_code = ?
+      ORDER BY id DESC
+      LIMIT 1
+      `,
+      [user_code]
+    );
+  
+    const no = lastTodoRow?.no + 1 || 1;
+  
+    const [insertTodoRs] = await pool.query(
+      `
+      INSERT INTO todo
+      SET reg_date = NOW(),
+      update_date = NOW(),
+      user_code = ?,
+      no = ?,
+      content = ?,
+      perform_date = ?,
+      is_completed = ?
+      `,
+      [user_code, no, content, perform_date, is_completed]
+    );
+  
+    const [justCreatedTodoRow] = await pool.query(
+      `
+      SELECT *
+      FROM todo
+      WHERE id = ?
+      `,
+      [insertTodoRs.insertId]
+    );
+  
+    res.json({
+      resultCode: "S-1",
+      msg: `${justCreatedTodoRow.id}번 할 일을 생성하였습니다.`,
+      data: justCreatedTodoRow,
+    });
+  });
+  
   
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
